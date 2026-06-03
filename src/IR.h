df83e0ea5f711f1a0dc9199f3a56bd902f387b3d
Jay Four Javier <165471235+jayfourjavier@users.noreply.github.com>
2026-06-03 20:14:59 +0800

    Update IR.h

--- src/IR.h
+++ src/IR.h
@@ -1,1 +1,47 @@
+
+// IR.h
+
+#pragma once
+
 #include <Arduino.h>
+
+#define IR_1_PIN 18
+#define IR_2_PIN 19
+#define IS_EMPTY_THRESHOLD_MILLIS 60000UL
+
+volatile unsigned long lastDropMillis = 0;
+volatile bool isEmpty = true;
+
+void IRAM_ATTR irTriggered()
+{
+    lastDropMillis = millis();
+    isEmpty = false;
+}
+
+inline void setupIR()
+{
+    pinMode(IR_1_PIN, INPUT_PULLUP);
+    pinMode(IR_2_PIN, INPUT_PULLUP);
+
+    lastDropMillis = millis();
+
+    attachInterrupt(
+        digitalPinToInterrupt(IR_1_PIN),
+        irTriggered,
+        FALLING
+    );
+
+    attachInterrupt(
+        digitalPinToInterrupt(IR_2_PIN),
+        irTriggered,
+        FALLING
+    );
+}
+
+inline void updateIR()
+{
+    if (millis() - lastDropMillis > IS_EMPTY_THRESHOLD_MILLIS)
+    {
+        isEmpty = true;
+    }
+}
